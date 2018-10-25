f02e3ec9b11e242d001a82b857befa70d4a930d6
Vincent-DeSousa-Tereso <Vincent.DeSousa.Tereso@ibm.com>
2018-10-24 23:31:58 -0400

    Wrong imports in main.go

--- main.go
+++ main.go
@@ -1,11 +1,11 @@
 package main
 
 import (
-	"fmt"
-	"pack1"
-	"pack2"
+	"github.com/vtereso/gopacker/pack1"
+	"github.com/vtereso/gopacker/pack2"
 )
 
 func main () {
-
+	pack1.setPack1()
+	pack2.getPack2()
 }
